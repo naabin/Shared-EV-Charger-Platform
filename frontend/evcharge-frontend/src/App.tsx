e198e1c446a508558389142816fd2caa084b0467
Nabin Karki <naabin@outlook.com>
2023-08-13 21:37:47 +1000

    project template started

---
+++ frontend/evcharge-frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+const BASE_URL = "http://127.0.0.1:8000/";
+function App() {
+  const [title, setTitle] = useState("");
+  useEffect(() => {
+    axios.get(BASE_URL).then((res) => {
+      setTitle(res.data.message);
+    });
+  });
+  return (
+    <div className="App">
+      <h1>{title && title}</h1>
+    </div>
+  );
+}
+
+export default App;
