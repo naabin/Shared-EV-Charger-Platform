da3e9a08eb0255cfb5eb3d53a3d0fb81e5e16382
Eren Zhang <hzha4878@uni.sydney.edu.au>
2023-09-16 19:48:09 +1000

    demo

---
+++ FrontDevelop/src/components/CoverPage/ModalForm.js
@@ -0,0 +1,82 @@
+import React from "react";
+import { Button, Checkbox, Form, Input, Modal } from "antd";
+
+function ModalForm(props) {
+  const handleOk = () => {};
+
+  return (
+    <Modal
+      title="Basic Modal"
+      open={props.showDialog}
+      onOk={handleOk}
+      onCancel={props.closeEvent}
+    >
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit" onClick={props.submit}>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+}
+
+export default ModalForm;
