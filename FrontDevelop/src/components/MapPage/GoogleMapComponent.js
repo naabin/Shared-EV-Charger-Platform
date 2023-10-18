1930492a9805a3086f56c44c2fdff7da0295dbf6
Eren Zhang <hzha4878@uni.sydney.edu.au>
2023-10-18 15:42:31 +1100

    add checkin button and link it to payment page

--- FrontDevelop/src/components/MapPage/GoogleMapComponent.js
+++ FrontDevelop/src/components/MapPage/GoogleMapComponent.js
@@ -4,6 +4,7 @@ import "../../styles/MapPage/GoogleMapComponent.css";
 import markerImage from "../../matirial/Image/charger.png";
 import axios from "axios";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Pay_Page from "../Pay_Page/Pay_Page";
 import {
   Accordion,
   AccordionDetails,
@@ -43,77 +44,6 @@ function GoogleMapComponent({
     return () => clearTimeout(timer);
   }, []);
 
-  const sampleChargers = [
-    {
-      charger_type: {
-        image: null,
-        name: "Fast Charger",
-        brand: "ACME",
-        power: "50 kW",
-        port_type: "Type 2",
-        amp: "125A",
-        warranty: 2,
-      },
-      address: {
-        street_address: "123 Main Street",
-        lat: -33.8688,
-        lng: 151.2093,
-        suburb: "Sydney",
-        post_code: "2000",
-        country: "Australia",
-      },
-      name: "Charger 1",
-      number_of_stars: 4,
-      number_of_rating: "100",
-      renter: 3,
-    },
-    {
-      charger_type: {
-        image: null,
-        name: "Standard Charger",
-        brand: "XYZ",
-        power: "22 kW",
-        port_type: "Type 1",
-        amp: "32A",
-        warranty: 1,
-      },
-      address: {
-        street_address: "456 Elm Street",
-        lat: -33.9167,
-        lng: 151.2333,
-        suburb: "Sydney",
-        post_code: "2000",
-        country: "Australia",
-      },
-      name: "Charger 2",
-      number_of_stars: 5,
-      number_of_rating: "50",
-      renter: 2,
-    },
-    {
-      charger_type: {
-        image: null,
-        name: "Ultra Charger",
-        brand: "FastCharge",
-        power: "100 kW",
-        port_type: "Type 3",
-        amp: "250A",
-        warranty: 3,
-      },
-      address: {
-        street_address: "789 Oak Avenue",
-        lat: -33.8679,
-        lng: 151.209,
-        suburb: "Sydney",
-        post_code: "2000",
-        country: "Australia",
-      },
-      name: "Charger 3",
-      number_of_stars: 3,
-      number_of_rating: "75",
-      renter: 1,
-    },
-  ];
 
   const handleMapClick = (e) => {
     console.log(e);
@@ -179,16 +109,23 @@ function GoogleMapComponent({
                 }
                 alt={selectedCharger.charger_type.image.name}
               />
+              <div className="checkinBtn-continer">
+                <button className="checkinBtn"><Pay_Page /></button>
+              </div>
+
               <Accordion>
+
                 <AccordionSummary
                   aria-controls="panel1-content"
                   expandIcon={<ExpandMoreIcon />}
                   id="panel1-header"
                 >
+
                   <Typography>
                     <strong> Charger Details </strong>
                   </Typography>
                 </AccordionSummary>
+
                 <AccordionDetails>
                   <p>
                     <strong>Type:</strong> {selectedCharger.charger_type.name}
